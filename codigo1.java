ad5374186d716b867084beed73b8aa77a84607bc
Jeffcus <jeff.05cus@gmail.com>
2023-09-02 12:29:31 -0500

    prueba

--- codigo1.java
+++ codigo1.java
@@ -1,6 +1,10 @@
 public class codigo1 {
     public static void main(String[] args) {
-        
+        if (6 < 6 * 5)
+        System.out.println("Hello");
+        System.out.println("There");
+
+
     }
     
 }
--- codigo2.java
+++ codigo2.java
@@ -1,6 +1,12 @@
-public class codigo1 {
+public class codigo2 {
     public static void main(String[] args) {
-        
+        int x=1,y=2,z=3;
+        if(x>y)
+        if(x>z)
+        System.out.println("1111");
+        else
+        System.out.println("2222");
+
     }
     
 }
